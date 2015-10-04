144ee44132d920ac8d8189c32ad07853119cd2e4
kkoenig <killiankoenig@gmail.com>
2015-10-03 22:02:49 -0700

    First arg of vector ctor should be num elements, second should be value. Fixes issue #121

--- tests/string_view_tests.cpp
+++ tests/string_view_tests.cpp
@@ -45,7 +45,7 @@ SUITE(string_view_tests)
 
     TEST(TestConstructFromStdVector)
     {
-        std::vector<char> vec('h', 5);
+        std::vector<char> vec(5, 'h');
         string_view<> v = vec;
         CHECK(v.length() == vec.size());
     }
