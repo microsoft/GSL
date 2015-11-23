38eaf9fc95149ccdff17c6c0a9f0fbed15fc41f8
Neil MacIntosh <neilmac@microsoft.com>
2015-11-23 15:07:41 -0800

    Refactored headers so span can use narrow_cast etc.

--- include/gsl.h
+++ include/gsl.h
@@ -20,6 +20,7 @@
 #define GSL_GSL_H
 
 #include "gsl_assert.h"  // Ensures/Expects
+#include "gsl_util.h"    // finally()/narrow()/narrow_cast()...
 #include "span.h"           // span, strided_span...
 #include "string_span.h"    // zstring, string_span, zstring_builder...
 #include <memory>
@@ -56,56 +57,6 @@ using std::shared_ptr;
 
 template <class T>
 using owner = T;
-
-//
-// GSL.util: utilities
-//
-
-// final_act allows you to ensure something gets run at the end of a scope
-template <class F>
-class final_act
-{
-public:
-    explicit final_act(F f) noexcept : f_(std::move(f)), invoke_(true) {}
-
-    final_act(final_act&& other) noexcept : f_(std::move(other.f_)), invoke_(other.invoke_) { other.invoke_ = false; }
-    final_act(const final_act&) = delete;
-    final_act& operator=(const final_act&) = delete;
-
-    ~final_act() noexcept { if (invoke_) f_(); }
-
-private:
-    F f_;
-    bool invoke_;
-};
-
-// finally() - convenience function to generate a final_act
-template <class F>
-final_act<F> finally(const F &f) noexcept { return final_act<F>(f); }
-
-template <class F>
-final_act<F> finally(F &&f) noexcept { return final_act<F>(std::forward<F>(f)); }
-
-// narrow_cast(): a searchable way to do narrowing casts of values
-template<class T, class U>
-T narrow_cast(U u) noexcept { return static_cast<T>(u); }
-
-struct narrowing_error : public std::exception {};
-// narrow() : a checked version of narrow_cast() that throws if the cast changed the value
-template<class T, class U>
-T narrow(U u) { T t = narrow_cast<T>(u); if (static_cast<U>(t) != u) throw narrowing_error(); return t; }
-
-//
-// at() - Bounds-checked way of accessing static arrays, std::array, std::vector
-//
-template <class T, size_t N> 
-T& at(T(&arr)[N], size_t index) { Expects(index < N); return arr[index]; }
-
-template <class T, size_t N>
-T& at(std::array<T, N>& arr, size_t index) { Expects(index < N); return arr[index]; }
-
-template <class Cont>
-typename Cont::value_type& at(Cont& cont, size_t index) { Expects(index < cont.size()); return cont[index]; }
 
 
 //
