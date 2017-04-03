979a989cc819c403d7882fb2a2655dfb0ded0d2f
Alexey Malov <alexey.malov@ispringsolutions.com>
2017-04-03 17:55:34 +0300

    tests for gsl::not_null comparison were added

--- tests/notnull_tests.cpp
+++ tests/notnull_tests.cpp
@@ -18,6 +18,7 @@
 #include <gsl/gsl>
 #include <vector>
 #include <memory>
+#include <string>
 
 using namespace gsl;
 
@@ -33,6 +34,54 @@ struct RefCounted
     operator T*() { return p_; }
     T* p_;
 };
+
+// user defined smart pointer with comparison operators returning non bool value
+template <typename T>
+struct CustomPtr
+{
+	CustomPtr(T* p) : p_(p) {}
+	operator T*() { return p_; }
+	bool operator !=(std::nullptr_t)const { return p_ != nullptr; }
+	T* p_ = nullptr;
+};
+
+template <typename T, typename U>
+std::string operator==(CustomPtr<T> const& lhs, CustomPtr<U> const& rhs)
+{
+    return reinterpret_cast<const void*>(lhs.p_) == reinterpret_cast<const void*>(rhs.p_) ? "true" : "false";
+}
+
+template <typename T, typename U>
+std::string operator!=(CustomPtr<T> const& lhs, CustomPtr<U> const& rhs)
+{
+	return reinterpret_cast<const void*>(lhs.p_) != reinterpret_cast<const void*>(rhs.p_) ? "true" : "false";
+}
+
+template <typename T, typename U>
+std::string operator<(CustomPtr<T> const& lhs, CustomPtr<U> const& rhs)
+{
+	return reinterpret_cast<const void*>(lhs.p_) < reinterpret_cast<const void*>(rhs.p_) ? "true" : "false";
+}
+
+template <typename T, typename U>
+std::string operator>(CustomPtr<T> const& lhs, CustomPtr<U> const& rhs)
+{
+	return reinterpret_cast<const void*>(lhs.p_) > reinterpret_cast<const void*>(rhs.p_) ? "true" : "false";
+}
+
+template <typename T, typename U>
+std::string operator<=(CustomPtr<T> const& lhs, CustomPtr<U> const& rhs)
+{
+	return reinterpret_cast<const void*>(lhs.p_) <= reinterpret_cast<const void*>(rhs.p_) ? "true" : "false";
+}
+
+template <typename T, typename U>
+std::string operator>=(CustomPtr<T> const& lhs, CustomPtr<U> const& rhs)
+{
+	return reinterpret_cast<const void*>(lhs.p_) >= reinterpret_cast<const void*>(rhs.p_) ? "true" : "false";
+}
+
+
 
 SUITE(NotNullTests)
 {
@@ -160,6 +209,40 @@ SUITE(NotNullTests)
         CHECK((NotNullSp1(sp1) >= NotNullSp2(sp2)) == (sp1 >= sp2));
         CHECK((NotNullSp2(sp2) >= NotNullSp1(sp1)) == (sp2 >= sp1));
     }
+
+	TEST(TestNotNullCustomPtrComparison)
+	{
+		int i = 42;
+		CustomPtr<int> p1(&i);
+		const int ci = 42;
+		CustomPtr<const int> p2(&ci);
+
+		using NotNull1 = not_null<decltype(p1)>;
+		using NotNull2 = not_null<decltype(p2)>;
+
+		CHECK((NotNull1(p1) == NotNull1(p1)) == "true");
+		CHECK((NotNull1(p1) == NotNull2(p2)) == "false");
+
+		CHECK((NotNull1(p1) != NotNull1(p1)) == "false");
+		CHECK((NotNull1(p1) != NotNull2(p2)) == "true");
+
+		CHECK((NotNull1(p1) < NotNull1(p1)) == "false");
+		CHECK((NotNull1(p1) < NotNull2(p2)) == (p1 < p2));
+		CHECK((NotNull2(p2) < NotNull1(p1)) == (p2 < p1));
+
+		CHECK((NotNull1(p1) > NotNull1(p1)) == "false");
+		CHECK((NotNull1(p1) > NotNull2(p2)) == (p1 > p2));
+		CHECK((NotNull2(p2) > NotNull1(p1)) == (p2 > p1));
+
+		CHECK((NotNull1(p1) <= NotNull1(p1)) == "true");
+		CHECK((NotNull1(p1) <= NotNull2(p2)) == (p1 <= p2));
+		CHECK((NotNull2(p2) <= NotNull1(p1)) == (p2 <= p1));
+
+		CHECK((NotNull1(p1) >= NotNull1(p1)) == "true");
+		CHECK((NotNull1(p1) >= NotNull2(p2)) == (p1 >= p2));
+		CHECK((NotNull2(p2) >= NotNull1(p1)) == (p2 >= p1));
+
+	}
 }
 
 int main(int, const char *[])
