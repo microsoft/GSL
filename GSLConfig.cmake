063b7c16a53ba067efca0c8d21af8b1e0682fa4a
Willy Scheibel <willy.scheibel@hpi.de>
2017-05-12 10:48:40 +0200

    Add missing find_module macro

--- GSLConfig.cmake
+++ GSLConfig.cmake
@@ -1,4 +1,11 @@
 
+# Macro to search for a specific module
+macro(find_module FILENAME)
+    if(EXISTS "${FILENAME}")
+        include("${FILENAME}")
+    endif()
+endmacro()
+
 find_module("${CMAKE_CURRENT_LIST_DIR}/cmake/GSL/GSL-export.cmake")
 
 set(GSL_FOUND Off)
