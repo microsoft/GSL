e4e32ed36e76bd618daa0329512ec0f8a39599a6
bashrc_real <saurabh8c@gmail.com>
2017-05-13 16:41:47 +0530

    Added comment about the new constraint on owner

--- tests/owner_tests.cpp
+++ tests/owner_tests.cpp
@@ -20,6 +20,8 @@
 
 #include <functional>
 
+#include <memory>
+
 using namespace gsl;
 
 SUITE(owner_tests)
@@ -39,7 +41,8 @@ SUITE(owner_tests)
     {
         #ifdef CONFIRM_COMPILATION_ERRORS
         {
-            owner<int> integer = 10;
+            owner<int> integerTest = 10;
+            owner<std::shared_ptr<int>> sharedPtrTest(new int(10));
         }
         #endif
     }
