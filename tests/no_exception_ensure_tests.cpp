d846fe50a3f0bb7767c7e087a05f4be95f4da0ec
Anna Gringauze <annagrin@microsoft.com>
2018-03-15 16:00:08 -0700

    Enable usage of gsl::narrow with exceptions disabled (#640)
    * Enable usage of gsl::narrow with exceptions disabled
    
    This solution uses the approach of boost::asio to enabling usage of the
    library in environments where exception usage is either prohibited
    or not feasible (due to code size constraints). A function template
    gsl::throw_exception has been added, which in a normal environment just
    throws the exception. However, when GSL_TERMINATE_ON_CONTRACT_VIOLATION
    is defined the function is only declared by gsl and the definition of
    this function template must be supplied by the library's user.
    
    Closes: #468
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>
    
    Addition:
    - understand STL no exception macro
    - use function static variable to set termination handler in kernel mode
    - add compile-only tests for no-exception mode
    
    * added termination tests and fixed bugs
    
    * disabled warning C4577 for msvc 2015

---
+++ tests/no_exception_ensure_tests.cpp
@@ -0,0 +1,53 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <cstdlib>      // for std::exit
+#include <gsl/span>     // for span
+
+int operator_subscript_no_throw()
+{
+    int arr[10]; 
+    gsl::span<int> sp { arr };
+    return sp[11];
+}
+
+
+void test_terminate()
+{ 
+    std::exit(0);
+}
+
+void setup_termination_handler()
+{
+#if defined(_MSC_VER)
+
+    auto& handler = gsl::details::get_terminate_handler();
+    handler = &test_terminate;
+
+#else
+
+    std::set_terminate(test_terminate);
+
+#endif
+}
+
+
+int main()
+{
+    setup_termination_handler();
+    operator_subscript_no_throw();
+    return -1;
+}
---
+++ tests/no_exception_throw_tests.cpp
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <cstdlib>      // for std::exit
+#include <gsl/gsl_util> // for narrow
+
+int narrow_no_throw()
+{
+    long long bigNumber = 0x0fffffffffffffff;
+    return gsl::narrow<int>(bigNumber); 
+}
+
+void test_terminate()
+{ 
+    std::exit(0);
+}
+
+void setup_termination_handler()
+{
+#if defined(_MSC_VER)
+
+    auto& handler = gsl::details::get_terminate_handler();
+    handler = &test_terminate;
+
+#else
+
+    std::set_terminate(test_terminate);
+
+#endif
+}
+
+
+int main()
+{
+    setup_termination_handler();
+    narrow_no_throw();
+    return -1;
+}
