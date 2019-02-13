f6d252d4a4bc1d7aa56654604600861d7ba31aa8
Daniel Donenfeld <dadonenf@microsoft.com>
2019-02-13 11:50:02 -0800

    Start updating asm process to be safe with parallel jobs. Push asm for each Job into its own branch

--- push_asm.bat
+++ push_asm.bat
@@ -1,4 +1,15 @@
+git config --global credential.helper store
+powershell -command 'Add-Content "$HOME\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"' > nul 2>&1
+git config --global user.email "dadonenf@microsoft.com"
+git config --global user.name "Daniel Donenfeld"
+git config --global core.autocrlf false 
+
+REM Get branch to check asm into 
 git checkout %APPVEYOR_REPO_BRANCH% 
+git checkout -b asm/%APPVEYOR_REPO_COMMIT%/appveyor-%APPVEYOR_JOB_NUMBER%
+
+REM Check asm into the branch
 git add "asm/*"
 git status
-git diff-index --cached --quiet --exit-code HEAD || (git commit -m "[skip ci] Update ASM for %ASM_FOLDER%" && git push)
+git diff-index --cached --quiet --exit-code HEAD || git commit -m "[skip ci] Update ASM for %ASM_FOLDER%"
+git push
