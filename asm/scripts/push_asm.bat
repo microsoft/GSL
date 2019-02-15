aacf0301fc0435a9659d3c405dd0266fcd8fafde
Daniel Donenfeld <dadonenf@microsoft.com>
2019-02-15 12:02:11 -0800

    redirect git to stdout so powershell doesn't see them as errors

--- asm/scripts/push_asm.bat
+++ asm/scripts/push_asm.bat
@@ -1,11 +1,11 @@
 git config --global core.autocrlf false 
 
 REM Get branch to check asm into 
-git checkout %APPVEYOR_REPO_BRANCH% 
-git checkout -b asm/%APPVEYOR_REPO_COMMIT%/appveyor-%APPVEYOR_JOB_ID%
+git checkout %APPVEYOR_REPO_BRANCH% 2>&1
+git checkout -b asm/%APPVEYOR_REPO_COMMIT%/appveyor-%APPVEYOR_JOB_ID% 2>&1
 
 REM Check asm into the branch
 git add "asm/*"
 git status
 git diff-index --cached --quiet --exit-code HEAD || git commit -m "[skip ci] Update ASM for %ASM_FOLDER%"
-git push -u origin HEAD
+git push -u origin HEAD 2>&1
