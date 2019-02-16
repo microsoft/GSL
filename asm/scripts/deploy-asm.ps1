096d572749e1072a6efc2466f1aaf55bbd7cf1c4
Daniel Donenfeld <dadonenf@microsoft.com>
2019-02-15 18:41:03 -0800

    Wrap noisy calls to git within cmd.exe invocation

--- asm/scripts/deploy-asm.ps1
+++ asm/scripts/deploy-asm.ps1
@@ -6,7 +6,7 @@ function collectAsm {
     git fetch --all 2>&1 
 
     $asmFinalBranch = "asm/$($env:APPVEYOR_REPO_COMMIT)/final"
-    git checkout -b $asmFinalBranch $env:APPVEYOR_REPO_BRANCH
+    cmd.exe /c "git checkout -b $($asmFinalBranch) $($env:APPVEYOR_REPO_BRANCH) 2>&1"
 
     #merge all branches into final branch
     (Get-AppVeyorBuild).build.jobs | Foreach-Object { 
@@ -18,19 +18,20 @@ function collectAsm {
             throw "Missing branch for job $($_.jobId)"
         }
 
-        git merge $branchName 2>&1
+        cmd.exe /c "git merge $($branchName) 2>&1"
+
     }
-    
+
     #Merge all branches into master
     git checkout master 2>&1
-    cmd.exe /c "git merge --squash $($asmFinalBranch)"
+    cmd.exe /c "git merge --squash $($asmFinalBranch) 2>&1"
     if(-not $?){
         throw "Failed merge"
     }
     git diff-index --cached --quiet --exit-code HEAD
     if(-not $?) {
         git commit -m "[skip ci] Update ASM for $($env:APPVEYOR_REPO_COMMIT)"
-        git push
+        cmd.exe /c "git push 2>&1"
     }
 }
 
