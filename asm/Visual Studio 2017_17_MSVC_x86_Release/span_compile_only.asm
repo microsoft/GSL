0c46d039acd8d64a7e2980e825d8329e2f623933
Daniel Donenfeld <dadonenf@microsoft.com>
2019-02-27 23:38:33 +0000

    [skip ci] Update ASM for cee06dcad9134819e267378a122f1de5bc2082ee

--- asm/Visual Studio 2017_17_MSVC_x86_Release/span_compile_only.asm
+++ asm/Visual Studio 2017_17_MSVC_x86_Release/span_compile_only.asm
@@ -286,8 +286,10 @@ __unwindtable$?foo@@YA_NXZ DD 0ffffffffH
 	DD	FLAT:___std_terminate
 	DD	0ffffffffH
 	DD	FLAT:___std_terminate
+	DD	0ffffffffH
+	DD	FLAT:___std_terminate
 __ehfuncinfo$?foo@@YA_NXZ DD 019930522H
-	DD	02H
+	DD	03H
 	DD	FLAT:__unwindtable$?foo@@YA_NXZ
 	DD	2 DUP(00H)
 	DD	2 DUP(00H)
