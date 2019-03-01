d4be64fe0f7a77228956d7ad216043840992c036
Daniel Donenfeld <dadonenf@microsoft.com>
2019-02-28 18:14:42 -0800

    Fix push_asm script to checkout the asm branches against the repo commit instead of the repo branch as the repo_branch could change if other users commit while the CI is running

--- asm/scripts/push_asm.bat
+++ asm/scripts/push_asm.bat
@@ -1,7 +1,7 @@
 git config --global core.autocrlf false 
 
 REM Get branch to check asm into 
-git checkout %APPVEYOR_REPO_BRANCH% 2>&1
+git checkout %APPVEYOR_REPO_COMMIT% 2>&1
 git checkout -b asm/%APPVEYOR_REPO_COMMIT%/appveyor-%APPVEYOR_JOB_ID% 2>&1
 
 REM Check asm into the branch
