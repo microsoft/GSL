1feb389ec33288d851d3a169b1d7995694409385
Werner Henze <w.henze@avm.de>
2023-02-06 16:00:40 +0100

    add not_null and strict_not_null tests for const

--- tests/notnull_tests.cpp
+++ tests/notnull_tests.cpp
@@ -442,11 +442,36 @@ TEST(notnull_tests, TestNotNullConstructorTypeDeduction)
     }
 
     {
+        const int i = 42;
+
+        not_null x{&i};
+#ifdef CONFIRM_COMPILATION_ERRORS
+        helper(not_null{&i});
+#endif
+        helper_const(not_null{&i});
+
+        EXPECT_TRUE(*x == 42);
+    }
+
+    {
         int i = 42;
         int* p = &i;
 
         not_null x{p};
         helper(not_null{p});
+        helper_const(not_null{p});
+
+        EXPECT_TRUE(*x == 42);
+    }
+
+    {
+        const int i = 42;
+        const int* p = &i;
+
+        not_null x{p};
+#ifdef CONFIRM_COMPILATION_ERRORS
+        helper(not_null{p});
+#endif
         helper_const(not_null{p});
 
         EXPECT_TRUE(*x == 42);
@@ -504,11 +529,36 @@ TEST(notnull_tests, TestMakeNotNull)
     }
 
     {
+        const int i = 42;
+
+        const auto x = make_not_null(&i);
+#ifdef CONFIRM_COMPILATION_ERRORS
+        helper(make_not_null(&i));
+#endif
+        helper_const(make_not_null(&i));
+
+        EXPECT_TRUE(*x == 42);
+    }
+
+    {
         int i = 42;
         int* p = &i;
 
         const auto x = make_not_null(p);
         helper(make_not_null(p));
+        helper_const(make_not_null(p));
+
+        EXPECT_TRUE(*x == 42);
+    }
+
+    {
+        const int i = 42;
+        const int* p = &i;
+
+        const auto x = make_not_null(p);
+#ifdef CONFIRM_COMPILATION_ERRORS
+        helper(make_not_null(p));
+#endif
         helper_const(make_not_null(p));
 
         EXPECT_TRUE(*x == 42);
@@ -556,15 +606,31 @@ TEST(notnull_tests, TestMakeNotNull)
 
 TEST(notnull_tests, TestStdHash)
 {
-    int x = 42;
-    int y = 99;
-    not_null<int*> nn{&x};
-    const not_null<int*> cnn{&x};
+    {
+        int x = 42;
+        int y = 99;
+        not_null<int*> nn{&x};
+        const not_null<int*> cnn{&x};
 
-    std::hash<not_null<int*>> hash_nn;
-    std::hash<int*> hash_intptr;
+        std::hash<not_null<int*>> hash_nn;
+        std::hash<int*> hash_intptr;
 
-    EXPECT_TRUE(hash_nn(nn) == hash_intptr(&x));
-    EXPECT_FALSE(hash_nn(nn) == hash_intptr(&y));
-    EXPECT_FALSE(hash_nn(nn) == hash_intptr(nullptr));
+        EXPECT_TRUE(hash_nn(nn) == hash_intptr(&x));
+        EXPECT_FALSE(hash_nn(nn) == hash_intptr(&y));
+        EXPECT_FALSE(hash_nn(nn) == hash_intptr(nullptr));
+    }
+
+    {
+        const int x = 42;
+        const int y = 99;
+        not_null<const int*> nn{&x};
+        const not_null<const int*> cnn{&x};
+
+        std::hash<not_null<const int*>> hash_nn;
+        std::hash<const int*> hash_intptr;
+
+        EXPECT_TRUE(hash_nn(nn) == hash_intptr(&x));
+        EXPECT_FALSE(hash_nn(nn) == hash_intptr(&y));
+        EXPECT_FALSE(hash_nn(nn) == hash_intptr(nullptr));
+    }
 }
