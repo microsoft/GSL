722b20b1788a73e23abf763e7d3c1e8444b47958
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2026-03-23 14:03:45 +0100

    gsl::not_null: Allow not_null (unique) pointers to be moved

--- tests/pointers_tests.cpp
+++ tests/pointers_tests.cpp
@@ -93,6 +93,105 @@ TEST(pointers_test, swap)
                   "!SwapCompilesFor<NotMoveAssignableCustomPtr>");
 }
 
+TEST(pointers_test, not_null_conversion_move_copy)
+{
+
+    struct MyBase
+    {
+        bool base{true};
+    };
+
+    struct MyDerived : MyBase
+    {
+        MyDerived() { base = false; }
+    };
+
+    {
+        gsl::not_null<std::shared_ptr<MyDerived>> nn_derived{std::make_shared<MyDerived>()};
+        gsl::not_null<std::shared_ptr<MyBase>> nn_base{std::make_shared<MyBase>()};
+
+        nn_base = nn_derived; // copy conversion
+        EXPECT_EQ(nn_base->base, false);
+
+        // get() const& returns a reference to the shared pointer
+        EXPECT_EQ(nn_base.get().use_count(), 2);
+        EXPECT_EQ(nn_derived.get().use_count(), 2);
+
+        // get()&& moves the shared pointer
+        {
+            const auto tmp{std::move(nn_base).get()};
+            EXPECT_EQ(tmp.use_count(), 2);
+        }
+        EXPECT_EQ(nn_derived.get().use_count(), 1);
+        EXPECT_EQ(nn_base.get(),
+                  nullptr); // Warning: In real code, a use-after-move is not allowed!
+
+        // Start fresh. Using nn_base is now allowed again.
+        nn_base = nn_derived; // copy conversion
+        EXPECT_EQ(nn_base->base, false);
+
+        // T() const& returns a copy of the shared pointer.
+        {
+            const std::shared_ptr<MyBase> tmp{nn_base};
+            EXPECT_EQ(tmp.use_count(), 3);
+        }
+        EXPECT_EQ(nn_derived.get().use_count(), 2);
+
+        // T()&& moves the shared pointer
+        {
+            const std::shared_ptr<MyBase> tmp{std::move(nn_base)};
+            EXPECT_EQ(tmp.use_count(), 2);
+        }
+        EXPECT_EQ(nn_derived.get().use_count(), 1);
+        EXPECT_EQ(nn_base.get(),
+                  nullptr); // Warning: In real code, a use-after-move is not allowed!
+    }
+    {
+        // Test the same with unique pointers and strict_not_null
+        gsl::strict_not_null<std::unique_ptr<MyDerived>> nn_derived{std::make_unique<MyDerived>()};
+        gsl::strict_not_null<std::unique_ptr<MyBase>> nn_base{std::make_unique<MyBase>()};
+
+        // nn_base = nn_derived; // does not compile:
+        static_assert(!std::is_copy_assignable<decltype(nn_base)>::value,
+                      "nn_base is not copy-assignable");
+        static_assert(!std::is_copy_constructible<decltype(nn_base)>::value,
+                      "nn_base is not copy-constructible");
+
+        nn_base = std::move(nn_derived);
+        EXPECT_EQ(nn_base->base, false);
+
+        // get() const& returns a reference
+        EXPECT_EQ(nn_base.get()->base, false);
+
+        // get()&& moves the unique pointer
+        {
+            const auto tmp{std::move(nn_base).get()};
+            EXPECT_EQ(tmp->base, false);
+        }
+        EXPECT_EQ(nn_base.get(),
+                  nullptr); // Warning: In real code, a use-after-move is not allowed!
+
+        // Start fresh. Using nn_base is now allowed again.
+        nn_base = gsl::strict_not_null<std::unique_ptr<MyBase>>{std::make_unique<MyDerived>()};
+        EXPECT_EQ(nn_base->base, false);
+
+        // T() const& returns a copy of the unique pointer, which does not compile.
+        {
+            static_assert(!std::is_copy_constructible<decltype(nn_base)::element_type>::value,
+                          "compilation of `const std::unique_ptr<MyBase>& tmp{nn_base};` is not "
+                          "possible because the element type is not copy constructible");
+        }
+
+        // T()&& moves the unique pointer
+        {
+            const std::unique_ptr<MyBase> tmp{std::move(nn_base)};
+            EXPECT_EQ(tmp->base, false);
+        }
+        EXPECT_EQ(nn_base.get(),
+                  nullptr); // Warning: In real code, a use-after-move is not allowed!
+    }
+}
+
 TEST(pointers_test, member_types)
 {
     static_assert(std::is_same<gsl::not_null<int*>::element_type, int*>::value,
